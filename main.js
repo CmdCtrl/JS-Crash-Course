643967795081a109f37816f58e0e68d082ee594b
CmdCtrl <thecmdctrl@gmail.com>
2020-07-19 18:10:07 -0400

    git setup

---
+++ main.js
@@ -0,0 +1,23 @@
+// Console commands.
+//console.log('Hello World');
+//console.error('This is an error');
+//console.warn('This is a warning');
+
+// Variables - var, let, const (for the most part you dont want to use var)
+// let - reasign values
+//const - constant immutable variable
+
+//Primitive Data Types - String, Numbers, Boolean, null, undefined
+const name = 'Jordan';
+const age = 30;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z; //also considered undefined
+
+//to test type 
+//console.log(typeof VARIABLE);
+
+// Concatenation - Template string
+console.log(`My name is ${name} and my age is ${age}`);
